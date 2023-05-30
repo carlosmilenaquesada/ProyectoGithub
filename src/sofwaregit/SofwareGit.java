e2b8063dd9cfa6b7349b6b1ec0a1f4acb2ba6900
carlos <micorreo@correo.com>
2023-05-30 16:50:13 +0200

    función resta

--- src/sofwaregit/SofwareGit.java
+++ src/sofwaregit/SofwareGit.java
@@ -23,7 +23,7 @@ public class SofwareGit {
         return num1+num2;
     }
     
-    public static int sumar(int num1, int num2){
-        return num1 + num2;
+    public static int restar(int num1, int num2){
+        return num1 - num2;
     }
 }
